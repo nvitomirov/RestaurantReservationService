039b67bba1be7583f5915f543bfa21adfdfe42d2
Nebojsa Vitomirov <nebojsa.vitomirov@gmail.com>
2022-01-27 17:28:28 +0100

    Reservation Controller annotation

--- src/main/java/com/nebovito/reservationservce/controller/RestaurantReservationController.java
+++ src/main/java/com/nebovito/reservationservce/controller/RestaurantReservationController.java
@@ -2,8 +2,8 @@ package com.nebovito.reservationservce.controller;
 
 import com.nebovito.reservationservce.model.dto.TableReservationCollectionDto;
 import com.nebovito.reservationservce.model.dto.TableReservationDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -11,14 +11,20 @@ import java.util.UUID;
 @RequestMapping("/reservations")
 public class RestaurantReservationController {
 
-    public TableReservationDto createReservation() {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public TableReservationDto createReservation(@RequestBody TableReservationDto tableReservation) {
         throw new UnsupportedOperationException();
     }
 
+    @DeleteMapping("/{id}")
     public void deleteReservation(UUID uuid) {
 
     }
 
+    @GetMapping
+    @ResponseBody
     public TableReservationCollectionDto readTableReservations() {
         throw new UnsupportedOperationException();
     }
